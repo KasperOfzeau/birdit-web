b005e6b9f3790b409edb1f24118703d51bbd923b
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-05-25 19:17:08 +0200

    Added send button

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-const message = document.querySelector("#message")
-const uploadButton = document.querySelector("#file")
-const img = document.querySelector("#img")
+const message = document.querySelector("#message");
+const uploadButton = document.querySelector("#file");
+const submitButton = document.querySelector('#submit');
+const img = document.querySelector("#img");
 
-uploadButton.addEventListener("change", event => loadFile(event))
-img.addEventListener("load", () => userImageUploaded())
+uploadButton.addEventListener("change", event => loadFile(event));
+submitButton.addEventListener("click", () => userImageUploaded());
 
 // Initialize the Image Classifier method with MobileNet
 const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
 
 function loadFile(event) {
-    img.src = URL.createObjectURL(event.target.files[0])
+    img.src = URL.createObjectURL(event.target.files[0]);
 }
 
 function userImageUploaded() {
-    message.innerHTML = "Image was loaded!"
     classifier.classify(img, (err, results) => {
         console.log(results);
         //message.innerHTML = `I think it's a ${results[0].label}!`
-        let imageResult = results[0].label
+        let imageResult = results[0].label;
 
         // Put the image result into the localstorage for future use
-        localStorage.setItem("imageResult", imageResult)
+        localStorage.setItem("imageResult", imageResult);
         window.location.href = "result.html";
     });
 }
@@ -28,5 +28,6 @@ function userImageUploaded() {
 // When the model is loaded
 function modelLoaded() {
     console.log('Model Loaded!');
-    message.innerHTML = "Please upload an image of a bird!"
+    submitButton.innerHTML = "Verzenden";
+    submitButton.removeAttribute("disabled");
 }
