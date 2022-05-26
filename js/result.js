6cbca397716b17be25978789af737657168de166
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-05-26 12:20:28 +0200

    display image

--- js/result.js
+++ js/result.js
@@ -2,6 +2,7 @@ let url = new URL(window.location.href); // Get url
 let search_params = url.searchParams; // Get params
 const id = search_params.get('id'); // Get id from params
 const message = document.querySelector("#message");
+const image = document.getElementById('image');
 
 // Get all saved predictions
 let listCollection = JSON.parse(localStorage.getItem("listCollection"));
@@ -11,5 +12,6 @@ for (const [key, value] of Object.entries(listCollection)) {
     // When the id is the same
     if(value.id == id){
         message.innerHTML = `I think it's a ${value.first_guess.label} with a confidence of: ${value.first_guess.confidence}`
+        image.src = "http://localhost:3000/uploads/" + value.imgName;
     }
-  }
+}
