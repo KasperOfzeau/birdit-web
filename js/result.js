324bd1085bb77fe59077abe75e284b4ab5dcc191
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-05-26 14:42:21 +0200

    Display info from info.json

--- js/result.js
+++ js/result.js
@@ -1,9 +1,15 @@
 let url = new URL(window.location.href); // Get url
 let search_params = url.searchParams; // Get params
 const id = search_params.get('id'); // Get id from params
-const message = document.querySelector("#message");
-const image = document.getElementById('image');
 
+const title = document.querySelector('#title');
+const message = document.querySelector("#message");
+const description = document.querySelector("#description");
+const facts = document.querySelector('#facts');
+const vogel = document.querySelector('.vogel');
+const help = document.querySelector('#help');
+const image = document.getElementById('image');
+let bird;
 // Get all saved predictions
 let listCollection = JSON.parse(localStorage.getItem("listCollection"));
 
@@ -11,7 +17,31 @@ let listCollection = JSON.parse(localStorage.getItem("listCollection"));
 for (const [key, value] of Object.entries(listCollection)) {
     // When the id is the same
     if(value.id == id){
-        message.innerHTML = `I think it's a ${value.first_guess.label} with a confidence of: ${value.first_guess.confidence}`
+        bird = value.first_guess.label;
+        title.innerHTML = bird;
+        vogel.innerHTML = bird;
+        message.innerHTML = `Je hebt een ${bird} op de foto gezet! Goed gedaan!`;
         image.src = "http://localhost:3000/uploads/" + value.imgName;
+        getJsonFile();
+    }
+}
+
+function getJsonFile() {
+    fetch("./info.json")
+    .then(res => res.json())
+    .then(data => getBirdInfo(data))
+}
+
+function getBirdInfo(infoJSON) {
+    for (const [key, value] of Object.entries(infoJSON)) {
+        if(value.bird == bird) {
+            description.innerHTML = value.description
+            for (const fact of value.facts) { 
+                let li = document.createElement("li");
+                li.innerHTML = fact;
+                facts.appendChild(li);
+            }
+            help.innerHTML = value.help;
+        }
     }
 }
